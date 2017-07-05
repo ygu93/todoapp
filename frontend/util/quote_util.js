f7809025e6e9aa5d320415b659b8abddda85641b
Yue Gu <yuegu1993@gmail.com>
2017-07-05 08:49:05 -0700

    update webpack

--- frontend/util/quote_util.js
+++ frontend/util/quote_util.js
@@ -1,7 +1,7 @@
 export const fetchQuote = (success) => {
   $.ajax({
     method:'GET',
-    url: `http://quotesondesign.com/wp-json/posts?filter[orderby]=rand&filter[posts_per_page]=1`,
+    url: `https://quotesondesign.com/wp-json/posts?filter[orderby]=rand&filter[posts_per_page]=1`,
     dataType:'json',
     cache: false,
     success
--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,29 @@
   const path = require('path');
 
+  var plugins = []; // if using any plugins for both dev and production
+  var devPlugins = []; // if using any plugins for development
+
+  var prodPlugins = [
+    new webpack.DefinePlugin({
+      'process.env': {
+        'NODE_ENV': JSON.stringify('production')
+      }
+    }),
+    new webpack.optimize.UglifyJsPlugin({
+      compress: {
+        warnings: true
+      }
+    })
+  ];
+
+  plugins = plugins.concat(
+    process.env.NODE_ENV === 'production' ? prodPlugins : devPlugins
+  )
+
   module.exports = {
     context: __dirname,
     entry: './frontend/todo.jsx',
+    plugins: plugins,
     output: {
       path: path.join(__dirname, 'app', 'assets', 'javascripts'),
       filename: 'bundle.js'
