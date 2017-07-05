abf47141e5e3eecfdd882388caed7c8d5c2a41e9
Yue Gu <yuegu1993@gmail.com>
2017-07-04 23:27:06 -0700

    remove obsolete redux files

--- frontend/middleware/todo_middleware.js
+++
@@ -1,8 +0,0 @@
-const TodoMiddleware = ({ dispatch }) => next => action => {
-  switch(action.type){
-    default:
-      return next(action);
-  }
-}
-
-export default TodoMiddleware
--- frontend/reducers/root_reducer.js
+++ frontend/reducers/root_reducer.js
@@ -1,6 +1,4 @@
 import { combineReducers } from 'redux';
-import TodoReducer from './todo_reducer.js';
-import StepReducer from './step_reducer.js';
 import { ApolloClient, createNetworkInterface } from 'react-apollo';
 
 const csrfToken = document.getElementsByName('csrf-token')[0].content
@@ -11,7 +9,5 @@ export const client = new ApolloClient({
 
 
 export const RootReducer = combineReducers ({
-  todo: TodoReducer,
-  step: StepReducer,
   apollo: client.reducer()
 });
